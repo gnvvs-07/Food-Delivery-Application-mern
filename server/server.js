19fe22ec7727ea721eb2f491e9d2f75674eba7ad
gokavarapu vinay sankar <vinaysankargokavarapu@gmail.com>
2024-05-28 18:44:15 +0530

    backend-sample API running

---
+++ server/server.js
@@ -0,0 +1,15 @@
+import express from "express";
+import cors from "cors";
+// app config
+const app = express();
+// port number
+const port = 4000;
+// middleware
+app.use(express.json());
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to my API");
+});
+
+app.listen(port,()=>{console.log("Server is running")});
