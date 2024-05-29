7d23d93a7d94b2149340e0f64ec425674b6f5e4e
gokavarapu vinay sankar <vinaysankargokavarapu@gmail.com>
2024-05-29 23:49:35 +0530

    add food server connected to front end - completed

--- admin/src/pages/Add/Add.jsx
+++ admin/src/pages/Add/Add.jsx
@@ -1,7 +1,9 @@
 import React, { useState } from "react";
 import "./Add.css";
 import { assets } from "../../assets/assets";
+import axios from "axios";
 export default function Add() {
+  const url = "http://localhost:4000";
   const [image, setImage] = useState(false);
   const [data, setData] = useState({
     name: "",
@@ -17,9 +19,34 @@ export default function Add() {
       [name]: value,
     }));
   };
+
+  //   api call
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+    const formData = new FormData(); //collect the form data
+    formData.append("name", data.name);
+    formData.append("description", data.description);
+    formData.append("price", Number(data.price));
+    formData.append("category", data.category);
+    formData.append("image", image);
+    // api call
+    const response = await axios.post(`${url}/api/food/add`, formData);
+    if (response.data.success) {
+      setData({
+        name: "",
+        description: "",
+        price: "",
+        category: "Salad",
+      });
+      setImage(false);
+    } else {
+      console.log("Error occurred");
+    }
+  };
+
   return (
     <div className="add">
-      <form className="flex-col">
+      <form className="flex-col" onSubmit={onSubmitHandler}>
         <div className="add-img-upload flex-col">
           <p>Upload Image</p>
           <label htmlFor="image">
